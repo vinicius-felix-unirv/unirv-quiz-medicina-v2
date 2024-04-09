d85f543aa6dbd49f93341adf4b0525129a14a84a
jean <jean.c.o.junior@academico.unirv.edu.br>
2024-04-09 01:40:12 -0300

    'rerew'

---
+++ quiz-medicina-v2/quiz-medicina-v2/src/app/modules/inscricao/inscricao.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-inscricao',
+  templateUrl: './inscricao.component.html',
+  styleUrls: ['./inscricao.component.scss']
+})
+export class InscricaoComponent {
+
+}
