dfa46eb04f85ce402f7e1a4db281692f2736790f
admilson <bittencourta942@gmail.com>
2024-04-19 21:47:12 -0300

    snackBar finalizada

--- quiz-medicina-v2/quiz-medicina-v2/src/app/modules/login/login.component.ts
+++ quiz-medicina-v2/quiz-medicina-v2/src/app/modules/login/login.component.ts
@@ -2,6 +2,8 @@ import { Component, ViewEncapsulation } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { ResetPasswordDialogComponent } from './reset-password-dialog/reset-password-dialog.component';
 import { MatDialog } from '@angular/material/dialog';
+import { MatSnackBar,  MatSnackBarVerticalPosition } from '@angular/material/snack-bar';
+
 
 @Component({
   selector: 'app-login',
@@ -9,16 +11,18 @@ import { MatDialog } from '@angular/material/dialog';
   styleUrls: ['./login.component.scss'],
   encapsulation: ViewEncapsulation.None
 })
-export class LoginComponent {
 
+export class LoginComponent {
 
   // O que falta: fazer o submiti, mensagem de erro caso o email ou a senha não sejam validos
 
   hide = true;
   formData!: FormGroup;
   dialog: any;
-  constructor(private _formBuilder: FormBuilder, public _dialog: MatDialog){}
+
   
+
+  constructor(private _formBuilder: FormBuilder, public _dialog: MatDialog, private _snackBar: MatSnackBar){}
 
   private createFormData(): FormGroup {
     return this.formData = this._formBuilder.group({
@@ -50,6 +54,7 @@ export class LoginComponent {
       console.log(this.formData.value);
     }else{
       console.log("error");
+      this.openSnackBar('Usuário não encontrado', 'Fechar');
     }
   }
 
@@ -61,6 +66,16 @@ export class LoginComponent {
       console.log("fechou")
     });
   }
+
+  verticalPosition: MatSnackBarVerticalPosition = 'top'
+
+  openSnackBar(message: string, action: string) {
+    this._snackBar.open(message, action, {
+      verticalPosition: this.verticalPosition,
+      duration: 2000, 
+    });
+  }
+
 
 }
 
