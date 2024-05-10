153d0528d3c281dc8b616da08e00af7afa75836e
Pedro-Bittencourt-Costa <pedro.b.costa@academico.unirv.edu.br>
2024-05-10 13:17:48 -0300

    feat: add funções aos services

---
+++ quiz-medicina-v2/quiz-medicina-v2/src/app/models/quizAvaliativo.ts
@@ -0,0 +1,9 @@
+
+export class QuizAvaliativo{
+  id?: number;
+  titulo: string = '';
+  imagem: string = '';
+  status: boolean = false;
+  usuarioid: number = 0;
+  cursoid: number = 0;
+}
--- quiz-medicina-v2/quiz-medicina-v2/src/app/services/quiz/quiz.service.ts
+++ quiz-medicina-v2/quiz-medicina-v2/src/app/services/quiz/quiz.service.ts
@@ -2,6 +2,8 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { ServiceBase } from '../servicebase/base.service';
 import { Quiz } from 'src/app/models/quiz';
+import { Observable } from 'rxjs';
+import { API_CONFIG } from 'src/app/config/api.config';
 
 @Injectable({
   providedIn: 'root'
@@ -9,6 +11,10 @@ import { Quiz } from 'src/app/models/quiz';
 export class QuizService extends ServiceBase<Quiz> {
 
   constructor(private http: HttpClient) {
-    super(http, "usuarios");
+    super(http, "quiz");
+  }
+
+  getAllQuizByCursoId(cursoId: number, skip: number, take: number): Observable<Quiz[]>{
+    return this.httpClient.get<Quiz[]>(`${API_CONFIG.baseUrl}/cursos/${cursoId}/quiz/${skip}/${take}`);
   }
 }
