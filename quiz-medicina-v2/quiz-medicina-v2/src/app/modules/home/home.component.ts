c342982b65f21eecaa08fb8244974485a260d81c
Pedro-Bittencourt-Costa <pedro.b.costa@academico.unirv.edu.br>
2024-05-19 15:24:49 -0300

    feat: add o ngform no html

--- quiz-medicina-v2/quiz-medicina-v2/src/app/modules/home/home.component.ts
+++ quiz-medicina-v2/quiz-medicina-v2/src/app/modules/home/home.component.ts
@@ -1,4 +1,13 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+
+
+export interface MenuItem {
+  menuIcon: string,
+  menuName: string,
+  menuUrl: string,
+  roleUser: number[]
+}
 
 @Component({
   selector: 'app-home',
@@ -7,5 +16,20 @@ import { Component } from '@angular/core';
 })
 export class HomeComponent {
 
+  constructor(private router: Router){}
+
   value = 'Insirir pergunta'
+
+  menu: MenuItem[] = [
+    { menuIcon: "fa-solid fa-bars", menuName: "perguntas", menuUrl: "tela-perguntas", roleUser: [1] },
+    { menuIcon: "fa-solid fa-house", menuName: "home", menuUrl: "home", roleUser: [1] },
+    { menuIcon: "fa-solid fa-user", menuName: "cadastro", menuUrl: "inscricao", roleUser: [1] },
+  ];
+
+  public redirect(item: MenuItem): void {
+
+    this.router.navigate([item.menuUrl]);
+
+  }
+
 }
