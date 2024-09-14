83ee2d8387787dbdde8887dfc63eac2dec26cdac
Pedro-Bittencourt-Costa <119755420+PedroBittencourtCosta@users.noreply.github.com>
2024-09-14 17:42:17 -0300

    feat: setando o menu pela role do usuario

--- quiz-medicina-v2/quiz-medicina-v2/src/app/modules/home/home.component.ts
+++ quiz-medicina-v2/quiz-medicina-v2/src/app/modules/home/home.component.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
+import { LoginResponse } from 'src/app/models/loginResponse';
 import { AuthService } from 'src/app/security/auth.service';
+import { StorageService } from 'src/app/services/dados/storage.service';
 
 export interface MenuItem {
   menuIcon: string,
@@ -15,25 +17,38 @@ export interface MenuItem {
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent {
+[x: string]: any;
 
   constructor(
     private router: Router,
-    private authService: AuthService
+    private authService: AuthService,
+    private sessionStorage: StorageService
   ){}
 
+  ngOnInit() {
+    this.role = parseInt(this.sessionStorage.getSessionData<LoginResponse>('login_info')?.role!);
+
+    this.menu = this.createMenu();
+  }
+
+  role: number = 0;
   value = 'Insirir pergunta'
 
   menu: MenuItem[] = [
-    { menuIcon: "fa-solid fa-house", menuName: "Home", menuUrl: "home", roleUser: [1] },
-    { menuIcon: "fa-solid fa-play", menuName: "Play", menuUrl: "home/quiz-screen", roleUser: [1] },
-    { menuIcon: "fa-solid fa-user", menuName: "Ranking", menuUrl: "home/ranking", roleUser: [1] },
-    { menuIcon: "fa-solid fa-bars", menuName: "Quiz", menuUrl: "home/tela-criacao-quiz", roleUser: [1] },
+    { menuIcon: "fa-solid fa-house", menuName: "Home", menuUrl: "home", roleUser: [1, 2, 3] },
+    { menuIcon: "fa-solid fa-play", menuName: "Play", menuUrl: "home/quiz-screen", roleUser: [1, 2, 3] },
+    { menuIcon: "fa-solid fa-user", menuName: "Ranking", menuUrl: "home/ranking", roleUser: [1, 2, 3] },
+    { menuIcon: "fa-solid fa-bars", menuName: "Quiz", menuUrl: "home/tela-criacao-quiz", roleUser: [1, 2] },
   ];
 
   public redirect(item: MenuItem): void {
 
     this.router.navigate([item.menuUrl]);
 
+  }
+
+  public createMenu(): MenuItem[]{
+    return this.menu.filter(item => item.roleUser.includes(this.role)).map(item => item);
   }
 
   logout(): void {
